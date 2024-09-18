32c187d7a6429d9014b1b7304bf2da4cd786f975
tushimittal <104528110+tushimittal@users.noreply.github.com>
2024-09-18 13:03:10 +0530

    Time: 34 ms (91.16%), Space: 16.5 MB (32.99%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+         num_strings = [str(num) for num in nums]
+
+        # Sort strings based on concatenated values
+         num_strings.sort(key=lambda a: a * 10, reverse=True)
+
+        # Handle the case where the largest number is zero
+         if num_strings[0] == "0":
+            return "0"
+
+        # Concatenate sorted strings to form the largest number
+         return "".join(num_strings)
